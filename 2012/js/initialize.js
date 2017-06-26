763e5d55e90d0df04d17fe1aa1262a846a53dc0b
Lee <lee.summers@gmail.com>
2017-06-25 20:05:52 -0400

    MM2017

---
+++ 2012/js/initialize.js
@@ -0,0 +1,424 @@
+jQuery(function( $ ){			
+	
+	//Cufon.replace('div.photo-title');
+	Cufon.replace('.container div h1');
+	Cufon.replace('.container div h2');
+	Cufon.replace('#right li');
+	Cufon.replace('#navigation a.navigation');
+	
+	$.swapImage(".swapImage");
+	
+	$(function() {
+		//$.fn.media.mapFormat('avi','quicktime');
+    	// this one liner handles all the examples on this page
+        $('a.media').media( {autoplay: true, caption:false});
+    });
+			
+    $('#s1').cycle({
+		fx: 'fade',		   
+        timeout: 0, 
+        speed:   300,
+        startingSlide: 31 
+    });
+    $('#goto1').click(function() { 
+        $('#s1').cycle(0);
+		return false; 
+    });
+    $('#goto2').click(function() { 
+        $('#s1').cycle(1);
+        return false; 
+    });
+	$('#goto3').click(function() { 
+        $('#s1').cycle(2);
+        return false; 
+    });
+	$('#goto4').click(function() { 
+        $('#s1').cycle(3);
+		return false; 
+    });
+	$('#goto5').click(function() { 
+        $('#s1').cycle(4); 
+        return false; 
+    });
+	$('#goto6').click(function() { 
+        $('#s1').cycle(5); 
+        return false; 
+    });
+	$('#goto7').click(function() { 
+        $('#s1').cycle(6); 
+        return false; 
+    });
+	$('#goto8').click(function() { 
+        $('#s1').cycle(7); 
+        return false; 
+    });
+	$('#goto9').click(function() { 
+        $('#s1').cycle(8); 
+        return false; 
+    });
+	$('#goto10').click(function() { 
+        $('#s1').cycle(9); 
+        return false; 
+    });
+	$('#goto11').click(function() { 
+        $('#s1').cycle(10); 
+        return false; 
+    });
+	$('#goto12').click(function() { 
+        $('#s1').cycle(11); 
+        return false; 
+    });
+	$('#goto13').click(function() { 
+        $('#s1').cycle(12); 
+        return false; 
+    });
+	$('#goto14').click(function() { 
+        $('#s1').cycle(13); 
+        return false; 
+    });
+	$('#goto15').click(function() { 
+        $('#s1').cycle(14); 
+        return false; 
+    });
+	$('#goto16').click(function() { 
+        $('#s1').cycle(15); 
+        return false; 
+    });
+	$('#goto17').click(function() { 
+        $('#s1').cycle(16); 
+        return false; 
+    });
+	$('#goto18').click(function() { 
+        $('#s1').cycle(17); 
+        return false; 
+    });
+	$('#goto19').click(function() { 
+        $('#s1').cycle(18); 
+        return false; 
+    });
+	$('#goto20').click(function() { 
+        $('#s1').cycle(19); 
+        return false; 
+    });
+	$('#goto21').click(function() { 
+        $('#s1').cycle(20); 
+        return false; 
+    });
+	$('#goto22').click(function() { 
+        $('#s1').cycle(21); 
+        return false; 
+    });
+	$('#goto23').click(function() { 
+        $('#s1').cycle(22); 
+        return false; 
+    });
+	$('#goto24').click(function() { 
+        $('#s1').cycle(23); 
+        return false; 
+    });
+	$('#goto25').click(function() { 
+        $('#s1').cycle(24); 
+        return false; 
+    });
+	$('#goto26').click(function() { 
+        $('#s1').cycle(25); 
+        return false; 
+    });
+	$('#goto27').click(function() { 
+        $('#s1').cycle(26); 
+        return false; 
+    });
+	$('#goto28').click(function() { 
+        $('#s1').cycle(27); 
+        return false; 
+    });
+	$('#goto29').click(function() { 
+        $('#s1').cycle(28); 
+        return false; 
+    });
+	$('#goto30').click(function() { 
+        $('#s1').cycle(29); 
+        return false; 
+    });
+	$('#goto31').click(function() { 
+        $('#s1').cycle(30); 
+        return false; 
+    });
+	$('#goto32').click(function() { 
+        $('#s1').cycle(31); 
+        return false; 
+    });
+	$('#goto33').click(function() { 
+        $('#s1').cycle(32); 
+        return false; 
+    });
+	$('#goto34').click(function() { 
+        $('#s1').cycle(33); 
+        return false; 
+    });
+	$('#goto35').click(function() { 
+        $('#s1').cycle(34); 
+        return false; 
+    });
+	$('#goto36').click(function() { 
+        $('#s1').cycle(35); 
+        return false; 
+    });
+	$('#goto37').click(function() { 
+        $('#s1').cycle(36); 
+        return false; 
+    });
+	$('#goto38').click(function() { 
+        $('#s1').cycle(37); 
+        return false; 
+    });
+	$('#goto39').click(function() { 
+        $('#s1').cycle(38); 
+        return false; 
+    });
+	$('#goto40').click(function() { 
+        $('#s1').cycle(39); 
+        return false; 
+    });
+	$('#goto41').click(function() { 
+        $('#s1').cycle(40); 
+        return false; 
+    });
+	$('#goto42').click(function() { 
+        $('#s1').cycle(41); 
+        return false; 
+    });
+	$('#goto43').click(function() { 
+        $('#s1').cycle(42); 
+        return false; 
+    });
+	$('#goto44').click(function() { 
+        $('#s1').cycle(43); 
+        return false; 
+    });
+	$('#goto45').click(function() { 
+        $('#s1').cycle(44); 
+        return false; 
+    });
+	$('#goto46').click(function() { 
+        $('#s1').cycle(45); 
+        return false; 
+    });
+	$('#goto47').click(function() { 
+        $('#s1').cycle(46); 
+        return false; 
+    });
+	$('#goto48').click(function() { 
+        $('#s1').cycle(47); 
+        return false; 
+    });
+	$('#goto49').click(function() { 
+        $('#s1').cycle(48); 
+        return false; 
+    });
+	$('#goto50').click(function() { 
+        $('#s1').cycle(49); 
+        return false; 
+    });
+	$('#goto51').click(function() { 
+        $('#s1').cycle(50); 
+        return false; 
+    });
+	$('#goto52').click(function() { 
+        $('#s1').cycle(51); 
+        return false; 
+    });
+	$('#goto53').click(function() { 
+        $('#s1').cycle(52); 
+        return false; 
+    });
+	$('#goto54').click(function() { 
+        $('#s1').cycle(53); 
+        return false; 
+    });
+	$('#goto55').click(function() { 
+        $('#s1').cycle(54); 
+        return false; 
+    });
+	$('#goto56').click(function() { 
+        $('#s1').cycle(55); 
+        return false; 
+    });
+	$('#goto57').click(function() { 
+        $('#s1').cycle(56); 
+        return false; 
+    });
+	$('#goto58').click(function() { 
+        $('#s1').cycle(57); 
+        return false; 
+    });
+	$('#goto59').click(function() { 
+        $('#s1').cycle(58); 
+        return false; 
+    });
+	$('#goto60').click(function() { 
+        $('#s1').cycle(59); 
+        return false; 
+    });
+	$('#goto61').click(function() { 
+        $('#s1').cycle(60); 
+        return false; 
+    });
+	$('#goto62').click(function() { 
+        $('#s1').cycle(61); 
+        return false; 
+    });
+	$('#goto63').click(function() { 
+        $('#s1').cycle(62); 
+        return false; 
+    });
+	$('#goto64').click(function() { 
+        $('#s1').cycle(63); 
+        return false; 
+    });
+	$('#goto65').click(function() { 
+        $('#s1').cycle(64); 
+        return false; 
+    });
+	$('#goto66').click(function() { 
+        $('#s1').cycle(65); 
+        return false; 
+    });
+	$('#goto67').click(function() { 
+        $('#s1').cycle(66); 
+        return false; 
+    });
+	$('#goto68').click(function() { 
+        $('#s1').cycle(67); 
+        return false; 
+    });
+	$('#goto69').click(function() { 
+        $('#s1').cycle(68); 
+        return false; 
+    });
+	$('#goto70').click(function() { 
+        $('#s1').cycle(69); 
+        return false; 
+    });
+	$('#goto71').click(function() { 
+        $('#s1').cycle(70); 
+        return false; 
+    });
+	$('#goto72').click(function() { 
+        $('#s1').cycle(71); 
+        return false; 
+    });
+	$('#goto73').click(function() { 
+        $('#s1').cycle(72); 
+        return false; 
+    });
+	$('#goto74').click(function() { 
+        $('#s1').cycle(73); 
+        return false; 
+    });
+	$('#goto75').click(function() { 
+        $('#s1').cycle(74); 
+        return false; 
+    });
+	$('#goto76').click(function() { 
+        $('#s1').cycle(75); 
+        return false; 
+    });
+	$('#goto77').click(function() { 
+        $('#s1').cycle(76); 
+        return false; 
+    });
+	$('#goto78').click(function() { 
+        $('#s1').cycle(77); 
+        return false; 
+    });
+	$('#goto79').click(function() { 
+        $('#s1').cycle(78); 
+        return false; 
+    });
+	$('#goto80').click(function() { 
+        $('#s1').cycle(79); 
+        return false; 
+    });
+	$('#goto81').click(function() { 
+        $('#s1').cycle(80); 
+        return false; 
+    });
+	$('#goto82').click(function() { 
+        $('#s1').cycle(81); 
+        return false; 
+    });
+	$('#goto83').click(function() { 
+        $('#s1').cycle(82); 
+        return false; 
+    });
+	$('#goto84').click(function() { 
+        $('#s1').cycle(83); 
+        return false; 
+    });
+	$('#goto85').click(function() { 
+        $('#s1').cycle(84); 
+        return false; 
+    });
+	$('#goto86').click(function() { 
+        $('#s1').cycle(85); 
+        return false; 
+    });
+	$('#goto87').click(function() { 
+        $('#s1').cycle(86); 
+        return false; 
+    });
+	$('#goto88').click(function() { 
+        $('#s1').cycle(87); 
+        return false; 
+    });
+	$('#goto89').click(function() { 
+        $('#s1').cycle(88); 
+        return false; 
+    });
+	$('#goto90').click(function() { 
+        $('#s1').cycle(89); 
+        return false; 
+    });
+	$('#goto91').click(function() { 
+        $('#s1').cycle(90); 
+        return false; 
+    });
+	$('#goto92').click(function() { 
+        $('#s1').cycle(91); 
+        return false; 
+    });
+	$('#goto93').click(function() { 
+        $('#s1').cycle(92); 
+        return false; 
+    });
+	$('#goto94').click(function() { 
+        $('#s1').cycle(93); 
+        return false; 
+    });
+	$('#goto95').click(function() { 
+        $('#s1').cycle(94); 
+        return false; 
+    });
+	$('#goto96').click(function() { 
+        $('#s1').cycle(95); 
+        return false; 
+    });
+	$('#goto97').click(function() { 
+        $('#s1').cycle(96); 
+        return false; 
+    });
+	$('#goto98').click(function() { 
+        $('#s1').cycle(97); 
+        return false; 
+    });
+	$('#goto99').click(function() { 
+        $('#s1').cycle(98); 
+        return false; 
+    });
+	$('#goto100').click(function() { 
+        $('#s1').cycle(99); 
+        return false; 
+    });
+	
+});
